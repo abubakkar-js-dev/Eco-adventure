7271de6836d5941f902e58a4125dd68e50b62c89
abubakkar-js-dev <mdabubakkars182@gmail.com>
2024-11-18 17:16:59 +0600

    update: the country i like section

--- src/components/FavCountryCard.jsx
+++ src/components/FavCountryCard.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 
 const FavCountryCard = ({blog}) => {
+  const countryName = blog.location.split(',')[1] || blog.location.split(',')[0];
+  console.log(countryName);
     return (
       <div className="group relative w-full h-80 overflow-hidden cursor-pointer flex flex-col items-center justify-center">
         {/* Background Image */}
@@ -13,9 +15,9 @@ const FavCountryCard = ({blog}) => {
 
         {/* Content */}
         <div className="relative z-10 p-4 flex flex-col justify-center items-center">
-          <h2 className="text-white text-2xl md:text-3xl lg:text-4xl font-bold">Mexico</h2>
+          <h2 className="text-white text-2xl md:text-3xl lg:text-4xl font-bold">{countryName}</h2>
           <p className="text-white text-lg md:text-xl mt-2">
-            14 Reasons Why You Should Visit Mexico Now
+            {blog.includedItems.length} Reasons Why You Should Visit {countryName} Now
           </p>
           <span className="text-white text-xs opacity-80 mt-5 inline-block">
             1 DESTINATIONS
